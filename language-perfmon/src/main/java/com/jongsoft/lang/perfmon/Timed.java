d5250f51926f6b56730de890b5bb51e7cc0e8f8a
Jongerius, Gerben <g.jongerius@jong-soft.com>
2018-11-20 16:28:48 +0100

    Add sonar to the build system

---
+++ language-perfmon/src/main/java/com/jongsoft/lang/perfmon/Timed.java
@@ -0,0 +1,26 @@
+package com.jongsoft.lang.perfmon;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Timed {
+
+    /**
+     * The name of the entry begin timed by this annotation.
+     *
+     * @return  the actual name
+     */
+    String name();
+
+    /**
+     * The process to which this timed entry belongs to. This can be used to group several timed
+     * entries by a logical process.
+     * <p/>
+     * <b>Default: </b> "None"
+     *
+     * @return  the process name
+     */
+    String process() default "None";
+
+}
