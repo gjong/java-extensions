1bd6e72fca4274fccfb12eb96f4ab20e48cb8d99
Jongerius, Gerben <g.jongerius@jong-soft.com>
2018-12-05 10:54:33 +0100

    Add package documentations

---
+++ src/main/java/com/jongsoft/lang/control/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * The control package contains flow control interfaces. These can be used to direct the logical flow of your code easier.
+ *
+ * <p><strong>Current support control mechanisms</strong></p>
+ * <ul>
+ *     <li>{@linkplain com.jongsoft.lang.control.Optional}, for {@code null} safe access to elements</li>
+ *     <li>{@linkplain com.jongsoft.lang.control.Try}, for exception handling logic</li>
+ * </ul>
+ * @since 0.0.1
+ */
+package com.jongsoft.lang.control;
--- src/main/java/com/jongsoft/lang/package-info.java
+++ src/main/java/com/jongsoft/lang/package-info.java
@@ -1,13 +1,28 @@
 /**
- * The core package contains all needed interfaces by the other modules of the language extension library.
- * <p>
- *     The base interface for all classes in the library is the {@link com.jongsoft.lang.Value} interface.
- *     This interface dictates a certain interaction that promotes functional behaviour of classes.
- * </p>
- * <p>
- *     There is also an interface {@link com.jongsoft.lang.Presence} which lays the foundation for a purely
- *     functional implementation of the Java 8 {@link java.util.Optional}.
- * </p>
+ * This package contains all interfaces used through out the functional library.
+ *
+ * <table>
+ *     <caption>Main interfaces</caption>
+ *     <thead>
+ *         <tr><td>Interface</td><td>Description</td></tr>
+ *     </thead>
+ *     <tbody>
+ *         <tr><td>{@linkplain com.jongsoft.lang.Value}</td><td>A wrapper interface for any element</td></tr>
+ *         <tr><td>{@linkplain com.jongsoft.lang.Presence}</td><td>Indicating some functionality for present logic</td></tr>
+ *     </tbody>
+ * </table>
+ *
+ * <table>
+ *     <caption>Supporting interfaces</caption>
+ *     <thead>
+ *         <tr><td>Interface</td><td>Description</td></tr>
+ *     </thead>
+ *     <tbody>
+ *         <tr><td>{@linkplain com.jongsoft.lang.Filterable}</td><td>Indicating the implementation can be filtered</td></tr>
+ *         <tr><td>{@linkplain com.jongsoft.lang.Mappable}</td><td>Indicating the implementation can be mapped</td></tr>
+ *         <tr><td>{@linkplain com.jongsoft.lang.Streamable}</td><td>Indicating the implementation can provide a Java stream</td></tr>
+ *     </tbody>
+ * </table>
  *
  * @since 0.0.1
  */
