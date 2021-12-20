5b116221c8589e743fc989a7af5be0d79bb65f34
Jongerius, Gerben <g.jongerius@jong-soft.com>
2021-12-20 09:46:05 +0100

    Add operation to create a prefilled list with one value and a set number of elements.

--- src/main/java/com/jongsoft/lang/Collections.java
+++ src/main/java/com/jongsoft/lang/Collections.java
@@ -6,6 +6,9 @@ import com.jongsoft.lang.collection.support.AbstractIterator;
 
 import java.util.Comparator;
 import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Arrays.copyOf;
 
@@ -28,9 +31,9 @@ public class Collections {
     /**
      * Creates an {@link Sequence} containing exactly one element, being the one passed to this call.
      *
-     * @param element   the element to add to the new array
-     * @param <T>       the type of the element
-     * @return          the new array list
+     * @param element the element to add to the new array
+     * @param <T>     the type of the element
+     * @return the new array list
      */
     @SuppressWarnings("squid:S00100")
     public static <T> Sequence<T> List(T element) {
@@ -40,15 +43,14 @@ public class Collections {
     /**
      * Creates a new {@link Sequence} with the provided elements as the contents.
      *
-     * @param elements  the elements to add to the array
-     * @param <T>       the type that the elements represent
-     * @return          the new array list
-     *
+     * @param elements the elements to add to the array
+     * @param <T>      the type that the elements represent
+     * @return the new array list
      * @throws NullPointerException if {@code elements} is null
      */
     @SafeVarargs
     @SuppressWarnings("squid:S00100")
-    public static <T> Sequence<T> List(final T...elements) {
+    public static <T> Sequence<T> List(final T... elements) {
         Objects.requireNonNull(elements, "The provided elements cannot be null");
         return create(copyOf(elements, elements.length));
     }
@@ -56,9 +58,9 @@ public class Collections {
     /**
      * Creates a new {@link Sequence} with all the elements contained in the {@link Iterable}.
      *
-     * @param elements  the elements that should be in the new array
-     * @param <T>       the type of the elements
-     * @return          the new array
+     * @param elements the elements that should be in the new array
+     * @param <T>      the type of the elements
+     * @return the new array
      */
     @SuppressWarnings({"unchecked", "squid:S00100"})
     public static <T> Sequence<T> List(Iterable<? extends T> elements) {
@@ -70,9 +72,9 @@ public class Collections {
     /**
      * Create a new {@link Sequence} containing all the elements in the {@code iterator}.
      *
-     * @param iterator  the iterator to copy into the array
-     * @param <T>       the type of the elements
-     * @return          the newly created array
+     * @param iterator the iterator to copy into the array
+     * @param <T>      the type of the elements
+     * @return the newly created array
      * @throws NullPointerException if {@code iterator} is null
      */
     @SuppressWarnings("squid:S00100")
@@ -82,15 +84,30 @@ public class Collections {
     }
 
     /**
+     * Create a new {@link Sequence} that is filled using the provided supplier.
+     *
+     * @param numberOfElement the number of elements to create a list with
+     * @param fillWith        the initial filling of the list
+     * @param <T>             the type of the element
+     * @return the newly created and filled list
+     * @since 1.2.0
+     */
+    public static <T> Sequence<T> List(int numberOfElement, Supplier<T> fillWith) {
+        return create(IntStream.range(0, numberOfElement)
+                .mapToObj(index -> fillWith.get())
+                .toArray());
+    }
+
+    /**
      * Create a new set containing all unique elements.
      *
-     * @param elements  the elements for the new set
-     * @param <T>       the type of the elements
-     * @return  the created set
+     * @param elements the elements for the new set
+     * @param <T>      the type of the elements
+     * @return the created set
      */
     @SafeVarargs
     @SuppressWarnings({"unchecked", "squid:S00100"})
-    public static <T> Set<T> Set(T...elements) {
+    public static <T> Set<T> Set(T... elements) {
         Set<T> newSet = EMPTY_SET;
 
         for (int i = 0; i < elements.length; i++) {
@@ -103,9 +120,9 @@ public class Collections {
     /**
      * Create a new set containing all the elements of the {@code iterable}.
      *
-     * @param iterable  the iterable containing the elements to add to the set
-     * @param <T>       the type of the elements
-     * @return  the created set
+     * @param iterable the iterable containing the elements to add to the set
+     * @param <T>      the type of the elements
+     * @return the created set
      * @throws NullPointerException if {@code iterable} is null
      */
     @SuppressWarnings({"unchecked", "squid:S00100"})
@@ -122,24 +139,24 @@ public class Collections {
     /**
      * Create a new sorted set with all the {@code elements} in it sorted by the provided {@code comparator}.
      *
-     * @param comparator    the comparator to use for sorting the set
-     * @param elements      the elements initially in the set
-     * @param <T>           the type of the elements
-     * @return              the newly generated sorted set
+     * @param comparator the comparator to use for sorting the set
+     * @param elements   the elements initially in the set
+     * @param <T>        the type of the elements
+     * @return the newly generated sorted set
      */
     @SafeVarargs
     @SuppressWarnings("squid:S00100")
-    public static <T> Set<T> Set(Comparator<T> comparator, T...elements) {
+    public static <T> Set<T> Set(Comparator<T> comparator, T... elements) {
         return Collections.Set(comparator, create(elements));
     }
 
     /**
      * Creates a new Sorted Set with the provided {@code comparator} and all elements that are in the provided {@code iterable}.
      *
-     * @param comparator    the comparator to use for sorting the set
-     * @param iterable      the iterable whose elements to copy into the new set
-     * @param <T>           the type of the elements
-     * @return              the newly generated sorted set
+     * @param comparator the comparator to use for sorting the set
+     * @param iterable   the iterable whose elements to copy into the new set
+     * @param <T>        the type of the elements
+     * @return the newly generated sorted set
      */
     @SuppressWarnings("squid:S00100")
     public static <T> Set<T> Set(Comparator<T> comparator, Iterable<? extends T> iterable) {
@@ -155,9 +172,9 @@ public class Collections {
     /**
      * Create a new empty {@link Map}.
      *
-     * @param <K>   the type of the key
-     * @param <T>   the type of the balue
-     * @return      the new empty map
+     * @param <K> the type of the key
+     * @param <T> the type of the balue
+     * @return the new empty map
      */
     @SuppressWarnings({"unchecked", "squid:S00100"})
     public static <K, T> Map<K, T> Map() {
@@ -167,11 +184,11 @@ public class Collections {
     /**
      * Create a new map with one pair in it being the provided {@code key}, {@code value}.
      *
-     * @param key       the key use for the first element
-     * @param value     the value use for the first element
-     * @param <K>       the type of the key
-     * @param <T>       the type of the balue
-     * @return          the new map
+     * @param key   the key use for the first element
+     * @param value the value use for the first element
+     * @param <K>   the type of the key
+     * @param <T>   the type of the balue
+     * @return the new map
      */
     @SuppressWarnings({"unchecked", "squid:S00100"})
     public static <K, T> Map<K, T> Map(K key, T value) {
@@ -181,12 +198,12 @@ public class Collections {
     /**
      * Creates a Iterator that contains all the provided elements.
      *
-     * @param elements  the elements to wrap in an iterator
-     * @param <T>       the type of the elements
-     * @return          the new iterator
+     * @param elements the elements to wrap in an iterator
+     * @param <T>      the type of the elements
+     * @return the new iterator
      */
     @SafeVarargs
-    public static <T> Iterator<T> Iterator(T...elements) {
+    public static <T> Iterator<T> Iterator(T... elements) {
         return new AbstractIterator<>() {
             private int index = 0;
 
@@ -210,8 +227,8 @@ public class Collections {
     /**
      * Create a new Iterator wrapping the provided iterable.
      *
-     * @param iterable  the iterable to wrap
-     * @param <T>       the type of elements in the iterable
+     * @param iterable the iterable to wrap
+     * @param <T>      the type of elements in the iterable
      * @return the iterator wrapping the iterable
      * @throws NullPointerException if {@code iterable} is null
      */
